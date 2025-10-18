7c66f8b3f4a53633dae4d28f1b4ce99a6a820c0e
mdhanifroni95 <eng.hanif.roni63@gmail.com>
2025-10-18 17:50:46 +0600

    sing up api complete

---
+++ apps/api/src/user/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { hash } from 'argon2';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...user } = createUserDto;
+      const hashPassword = await hash(password);
+      return await this.prisma.users.create({
+        data: {
+          password: hashPassword,
+          ...user,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async findByEmail(email: string) {
+    try {
+      return await this.prisma.users.findUnique({
+        where: {
+          email,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
---
+++ apps/web/app/auth/singup/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+const SingUpPage = () => {
+  return (
+    <div className="bg-white p-8 rounded-lg shadow-lg w-96 flex flex-col justify-center items-center">
+      <h1 className="text-center text-2xl font-bold mb-24">Sing Up Page</h1>
+
+      <div className="flex justify-between text-sm">
+        <p>Already have an account?</p>
+        <Link className="underline" href={"/auth/singin"}>
+          Sing in
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SingUpPage;
