6bc333f320c7d89bc843a7de7369d43a922fb589
mdhanifroni95 <eng.hanif.roni63@gmail.com>
2025-10-20 17:48:59 +0600

    add auth server in next js

--- apps/web/app/auth/singup/page.tsx
+++ apps/web/app/auth/singup/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import React from "react";
+import SingUpForm from "./singUpFrom";
 
 const SingUpPage = () => {
   return (
     <div className="bg-white p-8 rounded-lg shadow-lg w-96 flex flex-col justify-center items-center">
       <h1 className="text-center text-2xl font-bold mb-24">Sing Up Page</h1>
-
+      <SingUpForm />
       <div className="flex justify-between text-sm">
         <p>Already have an account?</p>
         <Link className="underline" href={"/auth/singin"}>
---
+++ apps/web/lib/auth.ts
@@ -0,0 +1,8 @@
+"use server";
+
+import { FormState } from "./type";
+
+export async function singUp(
+  state: FormState,
+  formData: FormData
+): Promise<FormState> {}
